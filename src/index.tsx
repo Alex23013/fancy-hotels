bf8f25348a1148241d77f369f95dfe82e1f576c8
Maria <maria@apurata.com>
2022-06-24 11:22:34 +0200

    adding strictMode

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
@@ -5,6 +6,8 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <App />
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
 
